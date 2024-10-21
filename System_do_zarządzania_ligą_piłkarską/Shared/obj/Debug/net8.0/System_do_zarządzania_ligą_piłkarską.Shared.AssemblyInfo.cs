1e5ff1cb4d788566179db6f144286200291ff0d7
danbur920 <116095790+danbur920@users.noreply.github.com>
2024-10-21 13:04:30 +0200

    added leaguemaster panel

--- System_do_zarządzania_ligą_piłkarską/Shared/obj/Debug/net8.0/System_do_zarządzania_ligą_piłkarską.Shared.AssemblyInfo.cs
+++ System_do_zarządzania_ligą_piłkarską/Shared/obj/Debug/net8.0/System_do_zarządzania_ligą_piłkarską.Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("System_do_zarządzania_ligą_piłkarską.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+107c472f21c4409eee4c100995ef5a792323a03a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3447ca7d74789b2266cda9d8c72bb5611f9024a")]
 [assembly: System.Reflection.AssemblyProductAttribute("System_do_zarządzania_ligą_piłkarską.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("System_do_zarządzania_ligą_piłkarską.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
